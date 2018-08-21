80128af6b7fe88d477c06ca530311dde98c0935f
Reece_Collett <rcollett@spartaglobal.com>
2018-08-21 08:49:11 +0100

    Finished the intro to Ruby

---
+++ variables.rb
@@ -0,0 +1,79 @@
+# # ~~ CREATING A VARIABLE ~~
+# my_variable = 5
+# puts my_variable
+
+# # ~~ SNAKE CASING ~~
+#   # Start with lowercase
+#   # More parts to the name require an underscore
+# a = 5
+# my_name = "Reece"
+# a_long_variable_name = 10
+
+# # Everything in ruby is an object
+
+# age = 5.5
+# puts age.ceil
+
+# # PHP Comparison
+
+# $age = 5.6;
+# echo ceil($age);
+
+# # ~~ NUMBERS ~~
+#
+# age = 20
+# current_year = 2017
+#
+# # Addition
+# age_in_ten_years = age + 10
+# puts age_in_ten_years
+#
+# # Subtraction
+# year_born = current_year - age
+# puts year_born
+#
+# # Multiplication
+# double_my_age = age * 2
+# puts double_my_age
+#
+# # Division
+# num_decades_old = age / 10
+# puts num_decades_old
+
+# ~~ DECIMALS ~~
+
+# # Make one of the numbers a decimal, then it will output as a decimal
+#
+# age = 22.0
+# current_year = 2017
+#
+# num_decades_old = age / 10
+# puts num_decades_old
+
+# ~~ STRINGS ~~
+
+# my_first_name = "Reece"
+# my_last_name = "Collett"
+#
+# my_full_name = my_first_name + " " + my_last_name
+# OR
+# This will work the same
+# my_full_name = "#{my_first_name} #{my_last_name}"
+# puts my_full_name
+
+# ~~ BUILT IN METHODS ~~
+
+# my_first_name = "Reece"
+# my_last_name = "Collett"
+#
+# # .reverse
+# puts my_first_name.reverse
+#
+# # .length
+# puts my_first_name.length
+#
+# # .upcase
+# puts my_first_name.upcase
+#
+# # .downcase
+# puts my_first_name.downcase
