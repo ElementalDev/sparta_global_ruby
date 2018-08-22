99825449b613b25eb106f904e3726706d6ad60ac
Reece_Collett <rcollett@spartaglobal.com>
2018-08-22 15:26:39 +0100

    Finished web application notes

---
+++ ruby_web_applications/rest_with_ruby.rb
@@ -0,0 +1,21 @@
+# REST - Representational state transfer
+
+# CRUD
+# CREATE | READ | UPDATE | DELETE
+
+# Normal API - get/cars/api - returns all the cars
+# RESTful API - get/cars/:id - returns the car with that specific id.
+
+# Get and entry - GET - eg get/cars/:id
+# Make a new entry - POST - eg fill out a form them post/cars
+# Update an entry with a form that already contains the data - PUT eg put/cars/:id get a form with data and then update it
+# Delete an entry - DELETE eg delete/cars/:id
+
+# rMVC
+# router
+# Model - PostgreSQL - RDBMS
+# View - ERB (Embedded Ruby)
+# Controller - Ruby
+
+# Sinatra will be the server
+# SQL for data
