f8910774edea3aa4f73a4bab412b434bad4c10d3
Reece_Collett <rcollett@spartaglobal.com>
2018-08-22 13:32:12 +0100

    Completed objects lesson

--- ruby_oop/intro/oop_intro.rb
+++ ruby_oop/intro/oop_intro.rb
@@ -1,55 +1,41 @@
 # How to include a class
 require_relative "person.rb"
 
-person1 = Person.new
+# Access properties and assign them or do as above and pass to constructor
+# person1.name = "Bob"
+# person1.sex = "Male"
+# person1.age = 55
+# person1.height = 1.65
+# person1.weight = 85
 
-# Access properties and assign them
-person1.name = "Bob"
-person1.sex = "Male"
-person1.age = 55
-person1.height = 1.65
-person1.weight = 85
+# Removed methods from main code and put them into Person class as they are performing calculations on the Person object
 
-def calculate_bmi(height, weight)
-  (weight / (height  ** 2)).to_i
-end
-
-def calculate_calories(sex, height, weight, age)
-  if sex == "Male"
-    bmr = 66.47 + (13.7 * weight) + (5 * height * 100) - (6.8 * age)
-  else
-    bmr = 655.1 + (9.6 * weight) + (1.8 * height * 100) - (4.7 * age)
-  end
-
-  bmr.to_i
-end
-#
+# BAD
 # # Person one
 # person_one_name = "Bob";
 # person_one_sex = "Male";
 # person_one_age = 50;
 # person_one_height = 1.86;
 # person_one_weight = 85;
-#
+
 # # Person two
 # person_two_name = "Fred";
 # person_two_sex = "Male";
 # person_two_age = 56;
 # person_two_height = 1.6;
 # person_two_weight = 87;
-#
-# calculate BMI
-person1_bmi = calculate_bmi(person1.height, person1.weight)
 
-# calculate required calories
-person1_calories = calculate_calories(person1.sex, person1.height, person1.weight, person1.age)
+# GOOD
+# Can pass values into the constructor
+person1 = Person.new("Bob", "Male", 55, 1.65, 85)
+person2 = Person.new("Fred", "Male", 56, 1.6, 87)
 
-puts "#{person1.name} has a BMI of #{person1_bmi}. Recommend calorie intake is #{person1_calories} calories"
+# Calculate required calories
+# WAS USING: person1_calories = calculate_calories(person1.sex, person1.weight, person1.height, person1.age) and the BMI one too
+# OOP Makes it look cleaner and makes reusability easier
 
-# # calculate BMI
-# person_two_bmi = calculate_bmi person_two_height, person_two_weight
-#
-# # calculate required calories
-# person_two_calories = calculate_calories(person_two_sex, person_two_height, person_two_weight, person_two_age)
-#
-# puts "#{person_two_name} has a BMI of #{person_two_bmi}. Recommend calorie intake is #{person_two_calories} calories"
+# Long way of doing it
+puts "#{person1.name} has a BMI of #{person1.calculate_bmi}. Recommend calorie intake is #{person1.calculate_calories} calories"
+puts "----------- Calling the description method in the person class -----------"
+# Short way of doing it with a print method within the class
+person2.description
--- ruby_oop/intro/person.rb
+++ ruby_oop/intro/person.rb
@@ -1,6 +1,70 @@
 # Classes start with a capital letter and are singular
 class Person
   # Variables within a class are called properties.
-  # attr_accessor creates the properties for us. We type them in as symbols.
+  # Attribute functions
+  # attr_accessor creates the properties for us and creates a getter and setter (read and write).
   attr_accessor :name, :sex, :age, :height, :weight
+  # # attr_reader creates the properties for us and creates just a getter (read only).
+  # attr_reader :name ...
+  # # attr_writer creates the properties for us and creates just a setter (write only).
+  # attr_writer :name ...
+
+  # Constructor
+  def initialize(name, sex, age, height, weight)
+    self.name = name
+    self.sex = sex
+    self.age = age
+    self.height = height
+    self.weight = weight
+  end
+
+  # Class methods
+  def calculate_bmi
+    (self.weight / (self.height  ** 2)).to_i
+  end
+
+  def calculate_calories
+    if self.sex == "Male"
+      bmr = 66.47 + (13.7 * self.weight) + (5 * self.height * 100) - (6.8 * self.age)
+    else
+      bmr = 655.1 + (9.6 * self.weight) + (1.8 * self.height * 100) - (4.7 * self.age)
+    end
+    bmr.to_i
+  end
+
+  def description
+    puts "#{self.name} has a BMI of #{self.calculate_bmi}. Recommend calorie intake is #{self.calculate_calories} calories"
+  end
+
+  # def initialize
+  #   # @ represents an instance variable
+  #   # These are not accessible outside of the class
+  #   @name
+  # end
+  #
+  # def say_name
+  #   puts @name
+  # end
+  #
+  # # This is what attr_accessor does in the background
+  # # Creates a private instance function. Eg @name, @sex, @age etc.
+  #
+  # # Getter function
+  # def get_name
+  #   @name
+  # end
+  # # TO USE GETTER: name = person1.get_name
+  #
+  # # Setter function
+  # def set_name(name)
+  #   @name = name
+  # end
+  # # TO USE THIS TYPE OF SETTER: person1.set_name("Reece")
+  #
+  # def name=(name)
+  #   @name = name
+  # end
+  # # TO USE THIS TYPE OF SETTER: person1.name = "Reece"
 end
+
+# Cant do this: person1.@name
